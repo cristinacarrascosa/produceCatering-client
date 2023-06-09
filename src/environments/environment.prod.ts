9b9ef90c5739ee571618fbc78ed55da1c9b9e9c6
cristinacarrascosa <cctausias@gmail.com>
2023-06-09 14:17:28 +0200

    ultimos toques

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,13 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
   production: true,
+};
 
-
-
+export const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json; charset=UTF-8',
+  }),
+  withCredentials: true
 };
 
 export const baseURL="producecatering-db-production.up.railway.app"
